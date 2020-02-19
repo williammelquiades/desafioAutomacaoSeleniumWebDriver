914e7c7fc0e239900000ffeb81ffa3d794d22fa3
William Melquiades <wms.melquiades@gmail.com>
2020-02-19 00:24:06 -0300

    Integrado Funcionalidade Gerenciar Projetos em Class Unica e Criacao de Class de teste com multi testes

--- CSharpSeleniumTemplate/Pages/GerenciarPage.cs
+++ CSharpSeleniumTemplate/Pages/GerenciarPage.cs
@@ -1,4 +1,5 @@
 ﻿using CSharpSeleniumTemplate.Bases;
+using CSharpSeleniumTemplate.Helpers;
 using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
@@ -13,7 +14,12 @@ namespace CSharpSeleniumTemplate.Pages
         //Abas de acesso em tela Gerenciar
         #region Mapping
         By abaInformacaoSite = By.XPath("(//ul[@class='nav nav-tabs padding-18']//li//a)[1]");
-        By textoNomeFormulario = By.XPath("//h4[@class='widget-title lighter']");
+        By infosInformeSite1 = By.XPath("(//*[@id='manage-overview-table']//tbody//tr/td)[1]");
+        By infosInformeSite2 = By.XPath("(//*[@id='manage-overview-table']//tbody//tr/td)[2]");
+        By infosInformeSite3 = By.XPath("(//*[@id='manage-overview-table']//tbody//tr/td)[4]");
+        By infosInformeSite4 = By.XPath("(//*[@id='manage-overview-table']//tbody//tr/td)[5]");
+        By textoPadraoFormulario = By.XPath("//h4[@class='widget-title lighter']");
+        By textoDubloFormulario = By.XPath("(//h4[@class='widget-title lighter'])[1]");
         By abaGerenciarUsuarios = By.LinkText("Gerenciar Usuários");
         By abaGerenciarProjetos = By.LinkText("Gerenciar Projetos");
         By abaGerenciarMarcadores = By.LinkText("Gerenciar Marcadores");
@@ -38,9 +44,32 @@ namespace CSharpSeleniumTemplate.Pages
         {
             Click(abaInformacaoSite);
         }
-        public string CapturarNomeDoFormulario()
+        public string CapturarPadraoNomeDoFormulario()
         {
-            return GetText(textoNomeFormulario);
+            return GetText(textoPadraoFormulario);
+        }
+
+        public string CapturarNomeDuploDoFormulario()
+        {
+            return GetText(textoDubloFormulario);
+        }
+
+        public string CapturarInfo1()
+        {
+            return GetText(infosInformeSite1);
+        }
+
+        public string CapturarInfo2()
+        {
+            return GetText(infosInformeSite2);
+        }
+        public string CapturarInfo3()
+        {
+            return GetText(infosInformeSite3);
+        }
+        public string CapturarInfo4()
+        {
+            return GetText(infosInformeSite4);
         }
 
         public void ClicarEmGerenciarUsuario()
@@ -71,11 +100,89 @@ namespace CSharpSeleniumTemplate.Pages
 
         public void ClicarEmAbaGerenciarPlugins()
         {
-            Click();
+            Click(abaGerenciarPlugins);
         }
         public void ClicarEmAbaGerenciarConfiguracao()
         {
-            Click();
+            Click(abaGerenciarConfiguracoes);
+        }
+
+        #endregion
+    }
+
+    public class FormularioGerenciarProjetosPage : PageBase
+    {
+        #region Mapping
+        By botaoNovoProjeto = By.XPath("//button[contains(text(),'Criar Novo Projeto') or contains(text(), 'Create New Project')]");
+        By nomeProjeto = By.Id("project-name");
+        By campoDescricao = By.Id("project-description");
+        By selecionaEstado = By.Id("project-status");
+        By selecionarVisibilidade = By.Id("project-view-state");
+        By botaoAdicionarProjeto = By.XPath("//input[@type='submit' or contains(text(), 'Adicionar projeto')]");
+        By mensagemErroTextArea = By.XPath("//*[@class='page-content']//p"); //*[starts-with(@class,'alert')]//p
+        By mensagemDeSucesso = By.XPath("//*[@class='page-content']//p");
+        //
+        By botãoAdicionarCategoria = By.XPath("//input[@type='submit' or contains(text(), 'Adicionar Categoria')]");
+        By campoNovaCategoria = By.ClassName("input-sm");
+        #endregion
+
+        #region Actions
+        //Gerenciamento Projeto: Seção >> Projetos
+       
+
+        public void ClicarEmNovoProjeto()
+        {
+            Click(botaoNovoProjeto);
+        }
+
+        public void PreencherNomeProjeto()
+        {
+            SendKeys(this.nomeProjeto, "Proj Test " + GeneralHelpers.ReturnStringWithRandomNumbers(2));
+        }
+
+        public void SelecionarEstado(string selecionaEstado)
+        {
+            ComboBoxSelectByVisibleText(this.selecionaEstado, selecionaEstado);
+        }
+
+        public void HerdarCategoria() { }
+
+        public void SelecionarVisibilidade(string selecionarVisibilidade)
+        {
+            ComboBoxSelectByVisibleText(this.selecionarVisibilidade, selecionarVisibilidade);
+        }
+
+        public void PreencherDescricao()
+        {
+            SendKeys(this.campoDescricao, "Descrição de teste automático" + GeneralHelpers.ReturnStringWithRandomCharacters(10));
+        }
+
+        public void ClicarEmAdicionarProjeto()
+        {
+            Click(botaoAdicionarProjeto);
+        }
+
+        public string RetornaMensagem()
+        {
+            return GetText(mensagemDeSucesso);
+        }
+
+        // Gerenciamento Projetos: Seção >> Categoria Global
+
+        public void PreencherCategoriaGlobal(string nomeCategoria)
+        {
+            SendKeys(this.campoNovaCategoria, nomeCategoria);
+            ;
+        }
+
+        public void ClicarAdicionarEmNovaCategoria()
+        {
+            Click(campoNovaCategoria);
+        }
+
+        public string ValidarCampoNomeProjeto(string msgJavaScripit)
+        {
+            return GetAttribute(this.nomeProjeto, msgJavaScripit);
         }
 
         #endregion
--- CSharpSeleniumTemplate/Tests/GerenciarTests.cs
+++ CSharpSeleniumTemplate/Tests/GerenciarTests.cs
@@ -20,6 +20,7 @@ namespace CSharpSeleniumTemplate.Tests
         [AutoInstance] LoginFlows loginInSystem;
         [AutoInstance] GerenciarPage gerenciarPage;
         [AutoInstance] MenuMantis menuMantis;
+        [AutoInstance] FormularioGerenciarProjetosPage formularioGerenciarProjeto;
         #endregion
 
         [Test]
@@ -28,6 +29,10 @@ namespace CSharpSeleniumTemplate.Tests
         {
             #region Parameters
             string nameEsperado = "Informação do Site";
+            string versao = "2.22.1";
+            string schema = "209";
+            string versaoPhp = "5.6.40";
+            string baseDados = "mysqli";
             #endregion
 
             loginInSystem.EfetuarLogin(USUARIO, SENHA);
@@ -38,7 +43,11 @@ namespace CSharpSeleniumTemplate.Tests
 
             Assert.Multiple(() =>
             {
-                Assert.AreEqual(nameEsperado, gerenciarPage.CapturarNomeDoFormulario());
+                Assert.AreEqual(nameEsperado, gerenciarPage.CapturarPadraoNomeDoFormulario());
+                Assert.AreEqual(versao, gerenciarPage.CapturarInfo1());
+                Assert.AreEqual(schema, gerenciarPage.CapturarInfo2());
+                Assert.AreEqual(versaoPhp, gerenciarPage.CapturarInfo3());
+                Assert.AreEqual(baseDados, gerenciarPage.CapturarInfo4());
             });
         }
 
@@ -56,7 +65,156 @@ namespace CSharpSeleniumTemplate.Tests
 
             gerenciarPage.ClicarEmGerenciarUsuario();
 
-            Assert.AreEqual(nameEsperado, gerenciarPage.CapturarNomeDoFormulario());
+            Assert.AreEqual(nameEsperado, gerenciarPage.CapturarNomeDuploDoFormulario());
+        }
+
+        [Test]
+        [Category("AcessarAbas")]
+        public void AcessarAbaGerenciarProjetos()
+        {
+            #region Parameters
+            string nameEsperado = "Projetos";
+            #endregion
+
+            loginInSystem.EfetuarLogin(USUARIO, SENHA);
+
+            menuMantis.ClicarItemMenuGerenciar();
+
+            gerenciarPage.ClicarEmAbaGerenciarProjetos();
+
+            Assert.AreEqual(nameEsperado, gerenciarPage.CapturarNomeDuploDoFormulario());
+        }
+
+        [Test]
+        [Category("AcessarAbas")]
+        public void AcessarAbaGerenciarMarcadores()
+        {
+            #region Parameters
+            string nameEsperado = "Gerenciar Marcadores";
+            #endregion
+
+            loginInSystem.EfetuarLogin(USUARIO, SENHA);
+
+            menuMantis.ClicarItemMenuGerenciar();
+
+            gerenciarPage.ClicarEmAbaGerenciarMarcadores();
+
+            Assert.AreEqual(nameEsperado, gerenciarPage.CapturarNomeDuploDoFormulario());
+        }
+
+        [Test]
+        [Category("AcessarAbas")]
+        public void AcessarAbaGerenciarCamposPersonalizados()
+        {
+            #region Parameters
+            string nameEsperado = "Campos Personalizados";
+            #endregion
+
+            loginInSystem.EfetuarLogin(USUARIO, SENHA);
+
+            menuMantis.ClicarItemMenuGerenciar();
+
+            gerenciarPage.ClicarEmAbaGerenciarCamposPersonalizados();
+
+            Assert.AreEqual(nameEsperado, gerenciarPage.CapturarPadraoNomeDoFormulario());
+        }
+
+        [Test]
+        [Category("AcessarAbas")]
+        public void AcessarAbaGerenciarPerfisGlobais()
+        {
+            #region Parameters
+            string nameEsperado = "Adicionar Perfil";
+            #endregion
+
+            loginInSystem.EfetuarLogin(USUARIO, SENHA);
+
+            menuMantis.ClicarItemMenuGerenciar();
+
+            gerenciarPage.ClicarEmAbaGerenciarPerfilGlobal();
+
+            Assert.AreEqual(nameEsperado, gerenciarPage.CapturarPadraoNomeDoFormulario());
+        }
+
+        [Test]
+        [Category("AcessarAbas")]
+        public void AcessarAbaGerenciarPlugins()
+        {
+            #region Parameters
+            string nameEsperado = "Plugins Instalados";
+            #endregion
+
+            loginInSystem.EfetuarLogin(USUARIO, SENHA);
+
+            menuMantis.ClicarItemMenuGerenciar();
+
+            gerenciarPage.ClicarEmAbaGerenciarPlugins();
+
+            Assert.AreEqual(nameEsperado, gerenciarPage.CapturarNomeDuploDoFormulario());
+        }
+
+        [Test]
+        [Category("AcessarAbas")]
+        public void AcessarAbaGerenciarConfiguracao()
+        {
+            #region Parameters
+            string nameEsperado = "ANEXO(S)";
+            #endregion
+
+            loginInSystem.EfetuarLogin(USUARIO, SENHA);
+
+            menuMantis.ClicarItemMenuGerenciar();
+
+            gerenciarPage.ClicarEmAbaGerenciarConfiguracao();
+
+            Assert.AreEqual(nameEsperado, gerenciarPage.CapturarNomeDuploDoFormulario());
+        }
+
+        [Test]
+        [Category("Campos Obrigatorios")]
+        public void ValidaCampoObrigatorioEmGerenciarProjetos()
+        {
+            #region  Parameters
+            string msgPT = "Preencha este campo.";
+            string msgEN = "Please fill out this field.";
+            string msgIE = "Este é um campo obrigatório";
+            string msgJavaScripit = "validationMessage";
+            #endregion
+
+            loginInSystem.EfetuarLogin(USUARIO, SENHA);
+
+            menuMantis.ClicarItemMenuGerenciar();
+
+            gerenciarPage.ClicarEmAbaGerenciarProjetos();
+
+            formularioGerenciarProjeto.ClicarEmNovoProjeto();
+
+            formularioGerenciarProjeto.ClicarEmAdicionarProjeto();
+
+            CollectionAssert.Contains(new[] { msgEN, msgPT, msgIE }, formularioGerenciarProjeto.ValidarCampoNomeProjeto(msgJavaScripit));
+        }
+
+        [Test]
+        public void CriarNovoProjetoComSucesso()
+        {
+
+            #region Parameters                 
+            string mensagemEsperada = "Operação realizada com sucesso.";
+            //    string caminhoArquivo = GeneralHelpers.ReturnProjectPath() + "Resources/Files/anexo_ocorrencia.jpg";
+            #endregion
+
+            loginInSystem.EfetuarLogin(USUARIO, SENHA);
+
+            menuMantis.ClicarItemMenuGerenciar();
+            gerenciarPage.ClicarEmAbaGerenciarProjetos();
+
+            formularioGerenciarProjeto.ClicarEmNovoProjeto();
+            formularioGerenciarProjeto.PreencherNomeProjeto();           
+            formularioGerenciarProjeto.PreencherDescricao();
+            formularioGerenciarProjeto.ClicarEmAdicionarProjeto();
+
+            Assert.AreEqual(mensagemEsperada, formularioGerenciarProjeto.RetornaMensagem());
+
         }
     }
 }
