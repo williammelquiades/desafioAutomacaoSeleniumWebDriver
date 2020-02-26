cfc3812ee2534568436942d301b5716fd8bcb3d4
William Melquiades <wms.melquiades@gmail.com>
2020-02-26 14:56:26 -0300

    Classes referente ao planejamento criados e iniciados

---
+++ CSharpSeleniumTemplate/Tests/PlanejamentoTests.cs
@@ -0,0 +1,33 @@
+﻿using CSharpSeleniumTemplate.Bases;
+using CSharpSeleniumTemplate.Flows;
+using CSharpSeleniumTemplate.Helpers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSeleniumTemplate.Tests
+{
+    [TestFixture]
+    public class PlanejamentoTests : TestBase
+    {
+        #region Pages and Flows Objects
+        string USUARIO = ConfigurationManager.AppSettings["username"].ToString();
+        string SENHA = ConfigurationManager.AppSettings["password"].ToString();
+        [AutoInstance] LoginFlows logarNoSistema;
+        [AutoInstance] MenuMantis menuMantis;
+        #endregion
+
+        [Test, Category("ValidarCampoObrigatorioResumo")]
+        public void ValidarCamposObrigatorioResumo()
+        {
+        
+        }
+
+
+
+    }
+}
