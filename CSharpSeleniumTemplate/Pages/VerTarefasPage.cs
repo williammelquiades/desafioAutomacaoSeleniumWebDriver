14d4405aa178c750075ca9e7f62487eb34983feb
William Melquiades <wms.melquiades@gmail.com>
2020-02-27 20:24:23 -0300

    Definicao de duas classes de testes iniciais

---
+++ CSharpSeleniumTemplate/Pages/VerTarefasPage.cs
@@ -0,0 +1,18 @@
+﻿using CSharpSeleniumTemplate.Bases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSeleniumTemplate.Pages
+{
+    public class VerTarefasPage : PageBase
+    {
+        #region Mapping
+        #endregion
+
+        #region Acction
+        #endregion
+    }
+}
---
+++ CSharpSeleniumTemplate/Tests/VerTarefasTests.cs
@@ -0,0 +1,45 @@
+﻿using CSharpSeleniumTemplate.Bases;
+using CSharpSeleniumTemplate.Flows;
+using CSharpSeleniumTemplate.Helpers;
+using CSharpSeleniumTemplate.Pages;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSeleniumTemplate.Tests
+{
+    [TestFixture]
+    public class VerTarefasTests : TestBase
+    {
+
+        #region Pages and Flows Objects
+        string USUARIO = ConfigurationManager.AppSettings["username"].ToString();
+        string SENHA = ConfigurationManager.AppSettings["password"].ToString();
+        [AutoInstance] LoginFlows loginInSystem;
+        [AutoInstance] MenuMantis menuMantis;
+        [AutoInstance] GerenciarPage emGerenciarProjetos;
+        #endregion
+
+        [Test]
+        [Category ("Ver Tarefas")]
+        public void PesquisarTaredaComString()
+        {
+
+        }
+
+        /*
+        [Test]
+        [Category ("Ver Tarefas")]
+        public void PesquisarTarefasInexistente()
+        {
+
+        }
+        */
+
+
+    }
+}
